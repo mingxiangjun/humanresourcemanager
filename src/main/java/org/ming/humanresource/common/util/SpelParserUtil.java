c0e7760228687d4b3d09d1470280e592acc4f4d2
mingxiangjun <mingxiangjun@yuewen.com>
2019-08-23 15:01:33 +0800

    添加方法注释

--- src/main/java/org/ming/humanresource/common/util/SpelParserUtil.java
+++ src/main/java/org/ming/humanresource/common/util/SpelParserUtil.java
@@ -14,8 +14,20 @@ import org.springframework.expression.spel.support.StandardEvaluationContext;
  * @create 2017-12-14 11:06
  */
 public class SpelParserUtil {
+
+    public SpelParserUtil() {
+        // Utility clazz default constructor
+    }
+
     private static ExpressionParser parser = new SpelExpressionParser();
 
+    /**
+     * 解析SpeEl表达式，获取值
+     * @param key El表达式：#id
+     * @param params 形参：方法声明里面的参数
+     * @param args 实际参数：传递的参数
+     * @return
+     */
     public static Object getParamArg(String key, String[] params, Object[] args){
         Expression expression = parser.parseExpression(key);
         EvaluationContext context = new StandardEvaluationContext();
